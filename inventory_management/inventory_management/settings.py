918e3c5504d166731db303c5ae424c8848e0b732
Yazeed Allabadi <allabadiyazeed67@gmail.com>
2025-05-01 19:06:29 +0300

    added github OAuth

--- inventory_management/inventory_management/settings.py
+++ inventory_management/inventory_management/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
     
     # for 2fa
     'django_otp',
